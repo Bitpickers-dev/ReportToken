a723b35f170b78c970f8b52106a0893b48bda613
IshimotoTakara <takara120208@gmail.com>
2020-10-07 15:52:45 +0900

    ダウンロード数が中央値以上のレポートに評価の傾斜をかける処理の追加&ソートの実装法の修正

--- contracts/Algorithm.sol
+++ contracts/Algorithm.sol
@@ -20,7 +20,6 @@ contract Algorithm {
     Report[] public reports;
     Report[] public reportsAbove;
     Report[] public reportsBelow;
-
     Hash[] public reportsRp;
 
     constructor() public {
@@ -28,19 +27,19 @@ contract Algorithm {
         reports.push(Report("0x45c79bbE964d68beC3c8BeCAB9d3A7b7f7e9dDcd", "R2", 700));
         reports.push(Report("0xA4682e519519f3D461DEEa60ed8f2A07d7ed7458", "R3", 800));
         reports.push(Report("0x1FC6F20f2296628a7663902144Fc2D4507cCb5BD", "R4", 900));
-        // reports.push(Report("0x703f3570f9E4cE14a9bcD4082E6f099c74033421", "R5", 500));
-        // reports.push(Report("0x82400A6a62150Dc6a29B966b5322b35069a9484d", "R6", 400));
-        // reports.push(Report("0x41f3307e3693FcC9AAECa1d877B992b27B14950f", "R7", 300));
-        // reports.push(Report("0x82C9EE99802C077F2BB13E0Fb391753dF875aC31", "R8", 200));
-        // reports.push(Report("0xDC65E1106A1123fc00851D2e9CD8a5eDcd911afE", "R9", 100));
-        // reports.push(Report("0x1B01Da83f3053D7BE8D06Aaf9a0A0f62EaBB6c53", "R21", 50));
+        reports.push(Report("0x703f3570f9E4cE14a9bcD4082E6f099c74033421", "R5", 500));
+        reports.push(Report("0x82400A6a62150Dc6a29B966b5322b35069a9484d", "R6", 400));
+        reports.push(Report("0x41f3307e3693FcC9AAECa1d877B992b27B14950f", "R7", 300));
+        reports.push(Report("0x82C9EE99802C077F2BB13E0Fb391753dF875aC31", "R8", 200));
+        reports.push(Report("0xDC65E1106A1123fc00851D2e9CD8a5eDcd911afE", "R9", 100));
+        reports.push(Report("0x1B01Da83f3053D7BE8D06Aaf9a0A0f62EaBB6c53", "R21", 50));
     }
 
     // HPF():High Pass Filter
-    function HPF() public returns (uint256) {
+    function HPF() public returns (Hash[] memory) {
         uint16 _median = 0;
         uint256 _n = reports.length;
-        uint256 _a = 0; 
+        uint256 _a = 0;
 
         // 中央値を求める処理
         if (_n % 2 == 0) {
@@ -52,19 +51,35 @@ contract Algorithm {
         // 中央値以上/未満でレポートを分けて、中央値以上のレポートの数を算出
         for (uint i = 0; i < _n; i++) {
             if (reports[i].downloads >= _median) {
-                _a++;
                 reportsAbove.push(Report(reports[i].userAddress, reports[i].reportHash, reports[i].downloads));
             } else {
                 reportsBelow.push(Report(reports[i].userAddress, reports[i].reportHash, reports[i].downloads));
             }
         }
 
-        // 中央値以下のアイテムはRP=0
+        // 中央値以未満のレポートはRP=0
         for (uint i = 0; i < reportsAbove.length; i++) {
             reportsRp.push(Hash(reportsBelow[i].reportHash, 0));
         }
-        _a = reportsRp[3].RP1;
-        return _a;
+
+        // 評価の傾斜をかける処理
+        _a = reportsAbove.length;
+        Report[] memory sortReportsAbove = reportsAbove;
+        if (_a <= 8) {
+            // ダウンロード数順にソート
+            bubbleSort(sortReportsAbove);
+            for (uint i = 0; i < _a; i++) {
+                reportsRp.push(Hash(sortReportsAbove[i].reportHash, 4 - i/2));
+            }
+        } else {
+            // ダウンロード数順にソート
+            bubbleSort(sortReportsAbove);
+            for (uint i = 0; i < _a; i++) {
+                reportsRp.push(Hash(sortReportsAbove[i].reportHash, 4 - 4*i/_a));
+            }
+        }
+        
+        return reportsRp;
     }
 
     // swap関数
@@ -75,7 +90,7 @@ contract Algorithm {
     }
 
     // バブルソート(降順)
-    function bubbleSort(Report[] memory items) public pure returns (Report[] memory) {
+    function bubbleSort(Report[] memory items) public pure {
         uint length = items.length;
         for(uint i = 0; i < length-1; i++) {
             for(uint j = i; j <length; j++) {
@@ -84,13 +99,6 @@ contract Algorithm {
                 }
             } 
         }
-    }
-
-    // ソートの実行
-    function reportsSort() public view returns(Report[] memory) {
-        Report[] memory r = reports;
-        bubbleSort(r);
-        return r;
     }
 
     function reportsPush(string memory _user, string memory _reporthash, uint16 _downloads) public {
