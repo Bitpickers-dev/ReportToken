b62c5e5e99be9c92a8dd85b6cc111e93fdb99829
ts 21 <shoukitsuda0310@gmail.com>
2020-10-09 11:20:15 +0900

    データ構造をmappingに変更

--- contracts/Algorithm.sol
+++ contracts/Algorithm.sol
@@ -18,6 +18,8 @@ contract Algorithm {
   RP1Table [] public RP1Array;
   RP2Table [] public RP2Array;
 
+  mapping(uint => User) public userMapping;
+
   constructor() public {
     //RP1用のデータ(レポートハッシュ、レポート名、DL数)
     reports.push(Report("0xc9472850C2bbEBC689b581b92A2E1A694235c9e5", "R1", 900));
@@ -31,7 +33,6 @@ contract Algorithm {
     reports.push(Report("0xDC65E1106A1123fc00851D2e9CD8a5eDcd911afE", "R9", 100));
     reports.push(Report("0x1B01Da83f3053D7BE8D06Aaf9a0A0f62EaBB6c53", "R21", 50));
 
-    //RP2の情報は別途用意する
     //RP2用のデータ(ユーザーアドレス、トークン購入量)
     //TODO:1:　データ構造をmappingに置き換える
     users.push(User(0xc9472850C2bbEBC689b581b92A2E1A694235c9e5, 100));
@@ -44,6 +45,10 @@ contract Algorithm {
     users.push(User(0x82C9EE99802C077F2BB13E0Fb391753dF875aC31, 30));
     users.push(User(0xDC65E1106A1123fc00851D2e9CD8a5eDcd911afE, 20));
     users.push(User(0x1B01Da83f3053D7BE8D06Aaf9a0A0f62EaBB6c53, 10));
+
+    userMapping[1].userAddress = 0xc9472850C2bbEBC689b581b92A2E1A694235c9e5;
+    userMapping[1].purchasedTokenAmount = 100;
+
   }
 
   // HPF():High Pass Filter
@@ -68,7 +73,11 @@ contract Algorithm {
 
     //アドレスごとのトークン発行量を計算
     for (uint i = 0; i < 10; i++) {
-      _totalIssuance += users[i].purchasedTokenAmount;
+      //構造体配列を用いる場合
+      //      _totalIssuance += users[i].purchasedTokenAmount;
+
+      //連想配列を用いる場合
+      _totalIssuance += userMapping[i].purchasedTokenAmount;
     }
 
     //ルーレットで当選者を5回きめる(数字はてきとー)
@@ -78,7 +87,11 @@ contract Algorithm {
       for (uint i = 0; i < 10; i++) {
         if (i == rp2Receiver[j]) break;
         {
-          address _receiverAddress = users[i].userAddress;
+          //構造体配列を用いる場合
+          //address _receiverAddress = users[i].userAddress;
+
+          //連想配列を用いる場合
+          address _receiverAddress = userMapping[i].userAddress;
           uint16 point = 0;
           point += 1;
           RP2Array.push(RP2Table(_receiverAddress, point));
@@ -102,7 +115,11 @@ contract Algorithm {
     uint8 k = 0;
 
     for (k = 0; k < 10; k++) {
-      uint256 _percentage = (users[k].purchasedTokenAmount / totalIssuance) * 100;
+      //構造体配列を用いる場合
+      //uint256 _percentage = (users[k].purchasedTokenAmount / totalIssuance) * 100;
+      
+      //連想配列を用いる場合
+      uint256 _percentage = (userMapping[k].purchasedTokenAmount / totalIssuance) * 100;
       _rangeMax += _percentage;
       if (_rangeMin <= hitNumber && _rangeMax >= hitNumber) break;
       _rangeMin = _rangeMax;
