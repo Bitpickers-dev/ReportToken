c33681ca07ada6d1e7b474d17a10dd585fb90136
IshimotoTakara <takara120208@gmail.com>
2020-11-04 05:16:17 +0900

    add the development environment for Cloud Functions

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
