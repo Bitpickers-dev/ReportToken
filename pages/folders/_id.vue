ce3dff037e9d9852e63ff694499825a61bf837cc
Nishino <nishino0719@icloud.com>
2020-12-13 07:32:59 +0900

    Update _id.vue

--- pages/folders/_id.vue
+++ pages/folders/_id.vue
@@ -5,8 +5,6 @@
     <div class="main-contents">
       <div class="main-content">
         <h1>レポート情報</h1>
-        <p>{{ shareUserAddress }}</p>
-        <p>レポートの識別子は{{ reportIndex }}</p>
         <div class="report-info">
           <Filecard :report="report" />
           <div class="report-table_info"></div>
@@ -14,20 +12,49 @@
             <h3>レポートの詳細</h3>
             <h5>{{ report.university }}</h5>
             <p class="report-exp">{{ report.detail }}</p>
-          </div>
-          <el-button v-if="!canWatch" @click="getReport"
-            >レポートを見る</el-button
-          >
-          <div class="" v-if="reportHash != null">
-            <h1>{{ reportHash }}</h1>
-            <a :href="`https://ipfs.io/ipfs/${reportHash}`" target="brank"
-              >レポートはこちら</a
+            <div class=""  v-if="
+                report.amount != 0 && report.amount > ownAmount / 100000000000
+              ">
+
+            <h4>
+              レポートの価格 :
+              {{ report.amount }}RPT<br />しかし、あなたが持っているのは{{
+                ownAmount / 100000000000
+              }}RPTです。
+            </h4>
+                <el-button type="primary">
+                  <nuxt-link to="/myPage" class="mypage-link"
+                    >トークン購入ページへ</nuxt-link
+                  >
+                </el-button>
+            </div>
+            <h4
+              v-if="
+                report.amount != 0 && report.amount <= ownAmount / 100000000000
+              "
             >
-            <img
+              レポートの価格 : {{ report.amount }}RPT<br />あなたは現在{{
+                ownAmount / 100000000000
+              }}RPT持っています
+            </h4>
+            <h4 v-if="report.amount == 0">
+              このレポートは無償で見ることができます！
+            </h4>
+          </div>
+          <div v-if="!canWatch">
+            <el-button
+              v-if="report != null && report.amount <= ownAmount / 100000000000"
+              @click="getReport"
+              >レポートを見る</el-button
+            >
+          </div>
+          <div class="" v-if="reportHash != null">
+            <iframe
               :src="`https://ipfs.io/ipfs/${reportHash}`"
               alt="共有したレポートの画像"
-              style="width: 20vw"
-            />
+              width="75%"
+              style="height: 1200px"
+            ></iframe>
           </div>
         </div>
       </div>
@@ -52,6 +79,9 @@ export default {
     this.shareUserAddress = reportId.slice(0, 42);
     let accounts = await this.$web3.eth.getAccounts();
     this.userAddress = accounts[0];
+    this.ownAmount = await this.$reportTokenContract.methods
+      .balanceOf(this.userAddress)
+      .call();
     await db
       .collection("reports")
       .where("shareUser", "==", this.shareUserAddress)
@@ -65,12 +95,23 @@ export default {
           }
         });
       });
+    if (this.report.amount == 0) {
+      this.canWatch = true;
+      let ret = await this.$reportInfoContract.methods
+        .getReport(this.reportIndex, this.shareUserAddress)
+        .call();
+      this.reportHash = ret;
+      console.log(
+        "買ってはいないけど、無償提供だハッシュ値は",
+        this.reportHash
+      );
+    }
     await db
       .collection("reports")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            this.allReports.push(doc.data());
+          this.allReports.push(doc.data());
         });
       });
     await db
@@ -84,9 +125,9 @@ export default {
         });
       });
     for (let i = 0; i < this.allReports.length; i++) {
-      if(this.buying.length > i){
-        console.log(this.report_doc)
-        console.log( this.buying[i].report_doc)
+      if (this.buying.length > i) {
+        console.log(this.report_doc);
+        console.log(this.buying[i].report_doc);
         if (this.report_doc == this.buying[i].report_doc) {
           this.canWatch = true;
           let ret = await this.$reportInfoContract.methods
@@ -98,19 +139,20 @@ export default {
       }
       if (this.shareUserAddress == this.userAddress) {
         this.canWatch = true;
-        console.log(this.reportIndex)
+        console.log(this.reportIndex);
         let ret = await this.$reportInfoContract.methods
           .getOwnerReport(this.reportIndex)
           .call();
-          console.log(ret)
+        console.log(ret);
         this.reportHash = ret;
-        console.log("fefefe",ret)
+        console.log("fefefe", ret);
         break;
       }
     }
   },
   data() {
     return {
+      ownAmount: null,
       canWatch: false,
       reportIndex: null,
       shareUserAddress: null,
@@ -123,7 +165,7 @@ export default {
       number: null,
       report_doc: null,
       buying: [],
-      allReports:[]
+      allReports: [],
     };
   },
   methods: {
@@ -134,7 +176,7 @@ export default {
     async getReport() {
       if (this.shareUserAddress != this.userAddress) {
         let decimals = await this.$web3.utils.toBN(18);
-        this.amount = 1000000000000;
+        this.amount = 100000000000 * this.report.amount;
         this.sendValue = await this.amount.valueOf(
           this.$web3.utils.toBN(10).pow(decimals)
         );
@@ -152,11 +194,14 @@ export default {
             buyAt: new Date(),
           });
 
-          // ダウンロード数が+1される
-        await db.collection('reports').doc(this.report_doc).update({
-          downloads: firebase.firestore.FieldValue.increment(1),
-          currentDownloads: firebase.firestore.FieldValue.increment(1),
-        })
+        // ダウンロード数が+1される
+        await db
+          .collection("reports")
+          .doc(this.report_doc)
+          .update({
+            downloads: firebase.firestore.FieldValue.increment(1),
+            currentDownloads: firebase.firestore.FieldValue.increment(1),
+          });
         let ret = await this.$reportInfoContract.methods
           .getReport(this.reportIndex, this.shareUserAddress)
           .call();
@@ -171,6 +216,12 @@ export default {
 <style>
 .report-info {
   text-align: center;
+}
+.mypage-link {
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+  padding: 8px;
 }
 .report-table_info {
   margin-left: 10%;
